cd28a6019cffe566a0be3b26040973452be2c021
SaraGarciaM <saragarciamatos@gmail.com>
2024-06-17 11:20:29 +0100

    Adds first solution to BST

--- AlgoExpert/easy.js
+++ AlgoExpert/easy.js
@@ -202,4 +202,24 @@ function transposeMatrix2(matrix) {
 
 //? Solution 1
 
-function findClosestValueInBst(tree, target) {}
+function findClosestValueInBst(tree, target) {
+  return findClosestValueInBstHelper(tree, target, Number.MAX_VALUE);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (tree === null) {
+    return closest;
+  }
+
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value;
+  }
+
+  if (target < tree.value) {
+    return findClosestValueInBstHelper(tree.left, target, closest);
+  } else if (target > tree.value) {
+    return findClosestValueInBstHelper(tree.right, target, closest);
+  } else {
+    return closest;
+  }
+}
